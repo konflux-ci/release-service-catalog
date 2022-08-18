cbc889fa0ab3a8fdfc79fb4a668ccb9a425ad3e5
parasense <jdisnard@redhat.com>
2022-08-18 10:52:18 -0500

    feat(hacbs-994): Change CI to use catalog dir (#47)
    * feat(hacbs-994): Change CI to use catalog dir
    
    Signed-off-by: Jon Disnard <jdisnard@redhat.com>

--- .github/scripts/tkn_push_bundles.sh
+++ .github/scripts/tkn_push_bundles.sh
@@ -3,51 +3,44 @@
 printf 'Gathering new/modified bundle definitions.\n'
 
 # Gather new/modified bundle definitions.
-declare -A bundle_dirs  # associative array to hash bundle dirs in key space.
+declare -A bundle_versioned_dirs  # associative array to hash bundle dirs in key space.
 for file in ${ALL_CHANGED_FILES}
 do
   printf 'MATCHED: %s\n' "$file"
-  bundle_dirs["${file%/*}"]=""  # hash the bundle dir
+  bundle_versioned_dirs["${file%/*}"]=""  # hash the bundle version dir
 done
 # EO gathering changed definitions
-printf 'bundle_dirs: %s\n' "${!bundle_dirs[@]}"
+printf 'bundle_versioned_dirs: %s\n' "${!bundle_versioned_dirs[@]}"
 
 # Main loop
 printf 'Pushing any new/modified bundle definitions.\n'
-for BUNDLE_DIR in "${!bundle_dirs[@]}"
+for VERSION_DIR in "${!bundle_versioned_dirs[@]}"
 do
-  if [[ ! -d "$BUNDLE_DIR" ]]
+  if [[ ! -d "$VERSION_DIR" ]]
   then
-    printf 'Skipping non-existing bundle-path: %s\n' "$BUNDLE_DIR"
+    printf 'Skipping non-existing bundle-path: %s\n' "$VERSION_DIR"
     continue
   fi
-  BUNDLE_FILES=()
-  BUNDLE_NAME=${BUNDLE_DIR##*/}
-  printf '* Bundle_dir: %s\n' $BUNDLE_DIR
-  printf '* Bundle name: %s\n' $BUNDLE_NAME
-  for file in $BUNDLE_DIR/*.yaml
-  do
-    [[ -e "$file" ]] || continue
-    printf '  * file: %s\n' "$file"
-    BUNDLE_FILES+=("$file")
-  done
+  # Note:
+  # 1: catalog dir
+  # 2: resource dir
+  # 3: name dir
+  # 4: version dir
+  shift $#
+  set -- ${VERSION_DIR////' '}  # Replace all forward-slash with white space
+  printf 'Resource: %s\nName: %s\nVersion: %s\n' "$2" "$3" "$4"
 
-  # xxx - tkn cli is apparently unable to process a list of bundle files on one `-f` cmdline arg.
-  # So we have to process `-f` for each definition file 1:1.
-  # (fix upstream)
-  printf -v bundle_files_args -- '-f %s ' ${BUNDLE_FILES[*]}
-  unset BUNDLE_FILES
+  # note: {registry}/{namespace}/{resource-type}-{name}
+  printf -v image_string '%s/%s/%s-%s' \
+    "$IMAGE_REGISTRY" \
+    "$IMAGE_NAMESPACE" \
+    "$2" \
+    "$3"
 
-  # note: {registry}/{namespace}/{repository}
-  printf -v image_string '%s/%s/%s' \
-    ${IMAGE_REGISTRY} \
-    ${IMAGE_NAMESPACE} \
-    "${BUNDLE_NAME}"
-
-    API_HTTP="https://${IMAGE_REGISTRY}/api/v1"
+  API_HTTP="https://${IMAGE_REGISTRY}/api/v1"
 
   # Test if the repo does not exist at {registry}/{namespace}.
-  if ! grep "${BUNDLE_NAME}" < <(
+  if ! grep "${2}-${3}" < <(
     curl \
       --silent \
       --request GET "${API_HTTP}/repository?namespace=${IMAGE_NAMESPACE}" \
@@ -58,7 +51,7 @@ do
     # Repo does not exist, so first create the repo.
     printf -v new_repo_string -- \
       '{"namespace": "%s", "repository": "%s", "description": "%s", "visibility": "%s", "repo_kind": "%s"}' \
-      "${IMAGE_NAMESPACE}" "${BUNDLE_NAME}" "${BUNDLE_NAME}" "public" "image"
+      "$IMAGE_NAMESPACE" "${2}-${3}" "${2}-${3}" "public" "image"
 
     printf 'Creating new repo: %s\n' "$image_string"
     curl \
@@ -69,10 +62,16 @@ do
       --data "$new_repo_string"
   fi
 
+  # Tag with git commit sha hex
   printf 'Pushing image to repo: %s:%s\n' "$image_string" "${GITHUB_SHA:0:7}"
-  tkn bundle push ${bundle_files_args} "${image_string}:${GITHUB_SHA:0:7}"
+  tkn bundle push -f "${VERSION_DIR}/${3}.yaml" "${image_string}:${GITHUB_SHA:0:7}"
 
+  # Tag with git branch string
   printf 'Pushing image to repo: %s:%s\n' "$image_string" "${GITHUB_REFNAME}"
-  tkn bundle push ${bundle_files_args} "${image_string}:${GITHUB_REFNAME}"
+  tkn bundle push -f "${VERSION_DIR}/${3}.yaml" "${image_string}:${GITHUB_REFNAME}"
+
+  # Tag with version dir string
+  printf 'Pushing image to repo: %s:%s\n' "$image_string" "${4}"
+  tkn bundle push -f "${VERSION_DIR}/${3}.yaml" "${image_string}:${4}"
 
 done
