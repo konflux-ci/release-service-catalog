c9178df5a53fe55cbeb142dc745761235ae2cf39
Ernesto González <ergonzal@redhat.com>
2023-12-04 23:19:30 +0100

    feat(RHTAP-1934): add sign-base64-blob task to release-to-github (#303)
    - Add new sign-base64-blob task with its tests, to sign a base64 encoded
      blob using the internal request script. Creating a .sig file
    - Update release-to-github pipeline to use this new task and release the
      .sig file as well
    - Update create-github-release task to also release the .sig file
    
    Signed-off-by: Ernesto González <ergonzal@redhat.com>

---
+++ tasks/sign-base64-blob/tests/mocks.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -eux
+
+# mocks to be injected into task step scripts
+function internal-request() {
+  echo Mock internal-request called with: $*
+  echo $* >> $(workspaces.data.path)/mock_internal-request.txt
+
+  # set to async
+  /home/utils/internal-request $@ -s false
+
+  # mimic the sync output
+  echo "Sync flag set to true. Waiting for the InternalRequest to be completed."
+  sleep 2
+}
+
+function kubectl() {
+  if [ $* != "get internalrequest internal-request -o=jsonpath='{.status.results.signed_payload}'" ]
+  then
+    echo "Unexpected call to kubectl"
+    exit 1
+  fi
+
+  echo "dummy-payload"
+}
---
+++ tasks/sign-base64-blob/tests/pre-apply-task-hook.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Install the CRDs so we can create/get them
+.github/scripts/install_crds.sh
+
+# Add RBAC so that the SA executing the tests can retrieve CRs
+kubectl apply -f .github/resources/crd_rbac.yaml
+
+# delete old InternalRequests
+kubectl delete internalrequests --all -A
+
+# Add mocks to the beginning of task step script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+yq -i '.spec.steps[0].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[0].script' $SCRIPT_DIR/../sign-base64-blob.yaml
