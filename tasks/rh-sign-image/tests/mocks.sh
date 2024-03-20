c4f71861973581ba655eabf685706d2a6b5d0248
Scott Hebert <scoheb@users.noreply.github.com>
2024-03-20 11:44:12 -0400

    feat(RHTAPREL-843): support signing of multi-arch images (#383)
    - we now detect if a reference image is multi-arch and then
      created InternalRequests for each arch digest found.
    
    Signed-off-by: Scott Hebert <scoheb@gmail.com>

--- tasks/rh-sign-image/tests/mocks.sh
+++ tasks/rh-sign-image/tests/mocks.sh
@@ -50,3 +50,82 @@ function set_ir_status() {
 EOF
     kubectl patch internalrequest $NAME --type=merge --subresource status --patch-file $PATCH_FILE
 }
+
+function skopeo() {
+  echo $* >> $(workspaces.data.path)/mock_skopeo.txt
+  if [[ "$*" == "inspect --raw docker://"* ]] || [[ "$*" == "inspect --no-tags --override-os linux --override-arch "*" docker://"* ]]
+  then
+    echo '{"mediaType": "my_media_type"}'
+  else
+    echo Mock skopeo called with: $*  >> /dev/stderr
+    if [[ "$*" != "inspect --no-tags docker://"* ]]
+    then
+      if [[ "$*" == "inspect --no-tags --raw docker://registry.io/multi-arch-image0"* ]]
+      then
+        echo '{
+                "schemaVersion": 2,
+                "mediaType": "application/vnd.oci.image.index.v1+json",
+                "manifests": [
+                  {
+                    "mediaType": "application/vnd.oci.image.manifest.v1+json",
+                    "digest": "sha256:6f9a420f660e73a",
+                    "size": 920,
+                    "platform": {
+                      "architecture": "amd64",
+                      "os": "linux"
+                    }
+                  },
+                  {
+                    "mediaType": "application/vnd.oci.image.manifest.v1+json",
+                    "digest": "sha256:6f9a420f660e73b",
+                    "size": 920,
+                    "platform": {
+                      "architecture": "arm64",
+                      "os": "linux"
+                    }
+                  }
+                ]
+              }
+            '
+      else
+        if [[ "$*" == "inspect --no-tags --raw docker://registry.io/image"* ]]
+        then
+          echo '{
+                  "schemaVersion": 2,
+                  "mediaType": "application/vnd.oci.image.manifest.v1+json",
+                  "config": {
+                    "mediaType": "application/vnd.oci.image.config.v1+json",
+                    "digest": "sha256:9e8f9c7bdce16d2e9ebf93b84d3f8df9821ab74f8c2bf73446e8828f936c9db1",
+                    "size": 11125
+                  },
+                  "layers": [
+                    {
+                      "mediaType": "application/vnd.oci.image.layer.v1.tar+gzip",
+                      "digest": "sha256:b4e744f5f131fb2db0dd7649806f286ecaa3fcda18dc9a4245d83e902100ccb3",
+                      "size": 78771642
+                    },
+                    {
+                      "mediaType": "application/vnd.oci.image.layer.v1.tar+gzip",
+                      "digest": "sha256:73a17d894da5e42b3c7d90bec673fc1a7d35ce351b2dd0fe9b82a5366f224456",
+                      "size": 174964243
+                    },
+                    {
+                      "mediaType": "application/vnd.oci.image.layer.v1.tar+gzip",
+                      "digest": "sha256:f9f79b486e0a53e4257f6d0baeb28bf8d662f5de03997e9eb7ccf658e4069c31",
+                      "size": 122857
+                    }
+                  ],
+                  "annotations": {
+                    "org.opencontainers.image.base.digest": "sha256:5ee218882a725fe3fcc8ebd803e82a7182dbee47aef0efcaf3852df9ad15347b",
+                    "org.opencontainers.image.base.name": "registry.access.redhat.com/ubi8/ubi:8.9-1028"
+                  }
+                }
+            '
+        else
+          echo Error: Unexpected call
+          exit 1
+        fi
+      fi
+    fi
+  fi
+}
