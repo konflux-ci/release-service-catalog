b6df1d198ec484fae1692cf2325b3ec4b779e9ce
Jindrich Luza <jluza@redhat.com>
2024-06-24 14:31:49 +0200

    pull secret args from secret process manifest digest in manifest list directly instead of manifest simplified code do not pass registry-user and password to cosign

--- tasks/rh-sign-cosign-image/tests/pre-apply-task-hook.sh
+++ tasks/rh-sign-cosign-image/tests/pre-apply-task-hook.sh
@@ -6,6 +6,15 @@
 # Add RBAC so that the SA executing the tests can retrieve CRs
 kubectl apply -f .github/resources/crd_rbac.yaml
 
+# Create a dummy slack-notification-secret secret (and delete it first if it exists)
+kubectl delete secret test-rh-sign-cosign-image-secret --ignore-not-found
+
+kubectl create secret generic test-rh-sign-cosign-image-secret\
+  --from-literal=AWS_REGION=us-test-1\
+  --from-literal=AWS_ACCESS_KEY_ID=test-access-key\
+  --from-literal=AWS_SECRET_ACCESS_KEY=test-secret-access-key
+
 # Add mocks to the beginning of task step script
+TASK_PATH="$1"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-yq -i '.spec.steps[0].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[0].script' $SCRIPT_DIR/../rh-sign-cosign-image.yaml
+yq -i '.spec.steps[0].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[0].script' "$TASK_PATH"
