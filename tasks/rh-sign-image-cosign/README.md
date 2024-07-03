d154b13f737497af5e8442748b536269e5fd5e77
Emily Zheng <yuzheng@redhat.com>
2024-07-03 08:39:30 +0800

    feat(CLOUDDST-22843): add rh-sign-image-cosign task
    This commit adds a task for signing container images by cosign.
    
    Signed-off-by: Emily Zheng <yuzheng@redhat.com>
    Co-authored-by: Jindrich Luza <jluza@redhat.com>

---
+++ tasks/rh-sign-image-cosign/README.md
@@ -0,0 +1,10 @@
+# rh-sign-image-cosign
+
+Tekton task to sign container images in snapshot by cosign.
+
+## Parameters
+
+| Name           | Description                                                               | Optional | Default value  |
+|----------------|---------------------------------------------------------------------------|----------|----------------|
+| snapshotPath   | Path to the JSON string of the mapped Snapshot spec in the data workspace | No       |                |
+| secretName     | Name of secret containing needed credentials                              | No       |                |
---
+++ tasks/rh-sign-image-cosign/tests/pre-apply-task-hook.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Create a dummy cosignSecretName secret (and delete it first if it exists)
+kubectl delete secret test-cosign-secret --ignore-not-found
+
+kubectl create secret generic test-cosign-secret\
+  --from-literal=AWS_REGION=us-test-1\
+  --from-literal=AWS_ACCESS_KEY_ID=test-access-key\
+  --from-literal=AWS_SECRET_ACCESS_KEY=test-secret-access-key\
+  --from-literal=SIGN_KEY=aws://arn:mykey
+
+# Add mocks to the beginning of task step script
+TASK_PATH="$1"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+yq -i '.spec.steps[0].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[0].script' "$TASK_PATH"
