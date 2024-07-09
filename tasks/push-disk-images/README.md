99217f7197b435e9b39a046a30515e40dbe5dc77
Johnny Bieren <jbieren@redhat.com>
2024-07-09 15:12:29 -0400

    feat(RELEASE-971): add task to push disk images
    This commit adds a task to push disk images via an InternalRequest. The
    steps done in the pull-disk-images task will be removed in a future
    commit as they are now done in the InternalRequest this task calls.
    
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

---
+++ tasks/push-disk-images/README.md
@@ -0,0 +1,11 @@
+# push-disk-images
+
+Tekton task to push disk images via an InternalRequest. The environment to use is pulled from the `cdn.env` key in the data file.
+
+## Parameters
+
+| Name                     | Description                                                                               | Optional | Default value               |
+|--------------------------|-------------------------------------------------------------------------------------------|----------|-----------------------------|
+| snapshotPath             | Path to the JSON file of the Snapshot spec in the data workspace                          | No       |                             |
+| dataPath                 | Path to data JSON in the data workspace                                                   | No       |                             |
+| pipelineRunUid           | The uid of the current pipelineRun. Used as a label value when creating internal requests | No       |                             |
---
+++ tasks/push-disk-images/tests/mocks.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -ex
+
+# mocks to be injected into task step scripts
+
+function internal-request() {
+  echo Mock internal-request called with: $*
+  echo $* >> $(workspaces.data.path)/mock_internal-request.txt
+
+  /home/utils/internal-request $@ -s false
+
+  if [[ "$*" == *'snapshot_json={"application":"disk-images","components":[{"name":"failing-disk-image"'* ]]; then
+      echo '{"result":"Failure"}' > $(workspaces.data.path)/mock_internal-request_result.txt
+  elif [[ "$*" == *"exodusGwEnv="@(live|pre)* ]]; then
+      echo '{"result":"Success"}' > $(workspaces.data.path)/mock_internal-request_result.txt
+  else
+      echo Unexpected call to internal-request
+      exit 1
+  fi
+}
+
+function kubectl() {
+  # The IR won't actually be acted upon, so mock it to return Success as the task wants
+  if [[ "$*" == "get internalrequest "*"-o=jsonpath={.status.results}" ]]
+  then
+    cat $(workspaces.data.path)/mock_internal-request_result.txt
+  else
+    /usr/bin/kubectl $*
+  fi
+}
