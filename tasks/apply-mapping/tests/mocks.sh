20abeea88dabba56604f64bb2832ecc91cacf20f
Johnny Bieren <jbieren@redhat.com>
2024-09-06 08:54:09 -0400

    feat(RELEASE-1116): timestamp variable is for build date (#547)
    This commit modifies the apply-mapping task to treat the timestamp user
    provided variable as the build timestamp instead of the current time. It
    is found by inspecting the image's labels. The existing timestamp
    functionality now exists under the release_timestamp variable.
    
    Also, if a tag is translated with a null value, the task will now fail.
    
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- tasks/apply-mapping/tests/mocks.sh
+++ tasks/apply-mapping/tests/mocks.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -eux
 
 # mocks to be injected into task step scripts
@@ -7,6 +7,9 @@ function date() {
   echo $* >> $(workspaces.config.path)/mock_date.txt
 
   case "$*" in
+      *"2024-07-29T02:17:29 +%Y-%m-%d")
+          echo "2024-07-29"
+          ;;
       *"+%Y%m%d %T")
           echo "19800101 00:00:00"
           ;;
@@ -24,4 +27,27 @@ function date() {
           exit 1
           ;;
   esac
+}
+
+function skopeo() {
+  echo Mock skopeo called with: $* >&2
+  echo $* >> $(workspaces.config.path)/mock_skopeo.txt
+
+  if [[ "$*" == "inspect --no-tags --override-os linux --override-arch amd64 docker://registry.io/badimage"* ]]
+  then
+    echo '{"Labels": {"not-a-build-date": "2024-07-29T02:17:29"}}'
+    return
+  elif [[ "$*" == "inspect --no-tags --override-os linux --override-arch amd64 docker://"* ]]
+  then
+    echo '{"Labels": {"build-date": "2024-07-29T02:17:29"}}'
+    return
+  fi
+
+  echo Error: Unexpected call
+  exit 1
+}
+
+function get-image-architectures() {
+    echo '{"platform":{"architecture": "amd64", "os": "linux"}, "digest": "abcdefg"}'
+    echo '{"platform":{"architecture": "ppc64le", "os": "linux"}, "digest": "deadbeef"}'
 }
