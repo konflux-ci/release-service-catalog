979cdfb76d853fa7de55938c8215e97ba12c3a83
Gang Li <ligangty@users.noreply.github.com>
2024-09-16 23:06:48 +0800

    feat(MMENG-4169): add publish-to-mrrc task (#510)
    This commit adds a task to publish artifacts from maven zip
    to MRRC(maven.repository.redhat.com)
    
    Signed-off-by: Gang Li <ligangty@users.noreply.github.com>

---
+++ tasks/publish-to-mrrc/tests/mocks.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -eux
+
+# mocks to be injected into task step scripts
+
+function oras(){
+  echo Mock oras called with: $*
+  echo $* >> $(workspaces.data.path)/mock_oras.txt
+
+  if [[ "$*" == "pull --registry-config"* ]]
+  then
+    echo Mock oras called with: $*
+    echo $4
+    IFS='/' arrIN=(${4}); unset IFS;
+    IFS='@' zip=(${arrIN[2]}); unset IFS;
+    touch /workdir/mrrc/$zip
+  else
+    echo Mock oras called with: $*
+    echo Error: Unexpected call
+    exit 1
+  fi
+}
+
+function charon(){
+  echo Mock charon called with: $*
+  echo $* >> $(workspaces.data.path)/mock_charon.txt
+
+  if [[ "$*" != "upload -p "*" -v "*" -t "*" "*"" ]]
+  then
+    echo Error: Unexpected call
+    exit 1
+  fi
+}
+
+function select-oci-auth() {
+  echo $* >> $(workspaces.data.path)/mock_select-oci-auth.txt
+}
+
