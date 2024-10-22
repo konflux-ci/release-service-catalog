a0d50ff38a398cca9e16c77393b5a4ad507431ea
Parthey Khanderia <60154758+pkhander@users.noreply.github.com>
2024-10-22 14:37:56 -0400

    feat(CLDX-79): add sign-mac-binaries step to sign-binaries task (#566)
    This commit adds sign-mac-binaries step to the sign-binaries task.
    
    Signed-off-by: Parthey Khanderia <pkhander@redhat.com>

--- tasks/sign-binaries/tests/mac_mocks.sh
+++ tasks/sign-binaries/tests/mac_mocks.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# This script specifically is used to specifically mock mac server calls
 
-count_file=$(workspaces.data.path)/win_ssh_count.txt
+count_file=$(workspaces.data.path)/mac_ssh_count.txt
 if [[ ! -f "$count_file" ]]; then
     echo "0" > "$count_file"
 fi
+
 
 function ssh() {
     # Read the current ssh_call_count from the file
@@ -12,10 +14,10 @@ function ssh() {
     ssh_call_count=$((ssh_call_count + 1))
     echo "$ssh_call_count" > "$count_file"
 
-    echo "$ssh_call_count" > "$(workspaces.data.path)/ssh_calls.txt"
+    echo "$ssh_call_count" > $(workspaces.data.path)/ssh_calls_mac.txt
 }
 
-scp_count_file="/tmp/scp_count.txt"
+scp_count_file="/tmp/scp_count_mac.txt"
 if [[ ! -f "$scp_count_file" ]]; then
     echo "0" > "$scp_count_file"
 fi
@@ -30,33 +32,22 @@ function scp() {
 
     scp_args=$@
 
-    # Check for Windows signing script
-    if [[ "$scp_args" == *"windows_signing_script"* ]]; then
-        echo "Windows SCP command detected with args: $@"
-        echo "$@" > $(workspaces.data.path)/scp_windows_args.txt
+    if [[ "$scp_args" == *"mac_signing_script"* ]]; then
+        echo "MAC SCP command detected with args: $@"
+        echo "$@" > $(workspaces.data.path)/scp_mac_args.txt
     fi
 
     if [[ "$scp_call_count" -eq 2 ]]; then
     echo "$@" > "$(workspaces.data.path)/mock_scp_2.txt"
     echo -n "sha256:0c4355ee4ef8d9d3875d5421972aed405ce6d8f5262983eeea3f6cbf5740c6e2" | \
     tee "$(results.signedWindowsDigest.path)"
-    echo -n "sha256:0c4355ee4ef8d9d3875d5421972aed405ce6d8f5262983eeea3f6cbf5740c6e2" | \
-    tee "$(results.signedMacDigest.path)"
     fi
 
 }
-
-oras_count_file="/tmp/oras_count.txt"
-if [[ ! -f "$count_file" ]]; then
-    echo "0" > "$count_file"
-fi
-
-
-function oras() {
-    # this is mocking the oras command to push unsigned binaries to the registry
-    echo "Digest: sha256:5ce6d8f5262983eeea3f6cbf5740c6e20c4355ee4ef8d9d3875d5421972aed40"
+function kinit() {
+    echo "kinit $@"
 }
 
-function kinit() {
-    echo "kinit $*"
+function oras() {
+    echo "oras $@"
 }
