f05f8ab306b4312afb406ba0af24e29ce84cdc71
Emily Zheng <yuzheng@redhat.com>
2024-10-22 17:17:21 +0800

    feat(CLOUDDST-24467): set rekor in cosign task (#620)
    Make it possible to enable rekor integration.
    
    Signed-off-by: Emily Zheng <yuzheng@redhat.com>

--- tasks/rh-sign-image-cosign/tests/pre-apply-task-hook.sh
+++ tasks/rh-sign-image-cosign/tests/pre-apply-task-hook.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 #
 # Create a dummy cosignSecretName secret (and delete it first if it exists)
-kubectl delete secret test-cosign-secret --ignore-not-found
+kubectl delete secret test-cosign-secret test-cosign-secret-rekor --ignore-not-found
 
 kubectl create secret generic test-cosign-secret\
   --from-literal=AWS_DEFAULT_REGION=us-test-1\
   --from-literal=AWS_ACCESS_KEY_ID=test-access-key\
   --from-literal=AWS_SECRET_ACCESS_KEY=test-secret-access-key\
   --from-literal=SIGN_KEY=aws://arn:mykey
+
+kubectl create secret generic test-cosign-secret-rekor\
+  --from-literal=AWS_DEFAULT_REGION=us-test-1\
+  --from-literal=AWS_ACCESS_KEY_ID=test-access-key\
+  --from-literal=AWS_SECRET_ACCESS_KEY=test-secret-access-key\
+  --from-literal=SIGN_KEY=aws://arn:mykey\
+  --from-literal=REKOR_URL=https://fake-rekor-server
 
 # Add mocks to the beginning of task step script
 TASK_PATH="$1"
