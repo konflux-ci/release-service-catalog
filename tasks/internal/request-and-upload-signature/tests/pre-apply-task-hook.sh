102832611ed9e7b7f504125b70dba7c88e83b660
Jindrich Luza <jluza@redhat.com>
2025-02-06 11:02:01 +0100

    feat(CLOUDDST-25758): use pubtools-sign sign and upload images
    internal request request-and-upload-signature was transfered to
    use pubtools-sign and pubtools-pyxis to sign and upload signatures.
    With that, internal requests are now called with multiple references and
    digests. Batch size is limited by params string length which is set by
    default to 4096. Retry mechanism in signing was also removed as it's
    already included in pubtools-sign
    
    Signed-off-by: Jindrich Luza <jluza@redhat.com>

--- tasks/internal/request-and-upload-signature/tests/pre-apply-task-hook.sh
+++ tasks/internal/request-and-upload-signature/tests/pre-apply-task-hook.sh
@@ -5,6 +5,9 @@ TASK_PATH="$1"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 yq -i '.spec.steps[0].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[0].script' "$TASK_PATH"
 yq -i '.spec.steps[1].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[1].script' "$TASK_PATH"
+yq -i '.spec.steps[2].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[2].script' "$TASK_PATH"
+yq -i '.spec.steps[3].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[3].script' "$TASK_PATH"
+yq -i '.spec.steps[4].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[4].script' "$TASK_PATH"
 
 # Create a dummy secret for ssl cert for pyxis interactions (and delete it first if it exists)
 kubectl delete secret pyxis-ssl-cert --ignore-not-found
