e3931daaf833f6eb0423635580b3e522ac52a4be
Martin Malina <mmalina@redhat.com>
2025-06-05 13:29:49 +0200

    fix(KONFLUX-7489): do not fail if we cannot close an issue
    * change th rh-advisories pipeline to not allow close-advisory-issues
      to fail
    * change close-advisory-issues so that it does not fail if it cannot
      get the transition id to close an issue or if closing the issue
      fails. If one of these fails, we will at least try to add
      a comment. But even if we cannot add the comment, we will not
      fail.
    
    For some projects we will not have the permissions to close issues,
    so intead of failing, we should just print a warning.
    
    Signed-off-by: Martin Malina <mmalina@redhat.com>

--- tasks/managed/close-advisory-issues/tests/mocks.sh
+++ tasks/managed/close-advisory-issues/tests/mocks.sh
@@ -11,16 +11,25 @@ function curl-with-retry() {
     :
   elif [[ "$*" == *"Content-Type"*"https://issues.redhat.com/rest/api/2/issue/FAIL-999/transitions" ]]
   then
-    exit 1
+    return 1
+  elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/FAIL-999/transitions" ]]
+  then
+    echo '{"transitions":[{"id":"91","name":"Closed","description":""},{"id":"11","name":"New"}]}'
   elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/ISSUE-123/transitions" ]]
   then
     echo '{"transitions":[{"id":"91","name":"Closed","description":""},{"id":"11","name":"New"}]}'
   elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/NOCLOSE-555/transitions" ]]
   then
-    exit 1
+    echo '{"expand":"transitions","transitions":[]}'
   elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/CLOSED-987" ]]
   then
     echo '{"fields":{"status":{"name":"Closed","id":"99"}}}'
+  elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/FAIL-999/comment" ]]
+  then
+    echo '{}'
+  elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/NOCLOSE-555/comment" ]]
+  then
+    echo '{}'
   else
     echo Error: Unexpected call
     exit 1
