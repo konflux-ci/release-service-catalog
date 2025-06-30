f379b0fe9d195cafbf1db1d3d0f60beaf5e51237
Martin Malina <mmalina@redhat.com>
2025-06-30 18:43:27 +0200

    fix: result too long in filter-already-released task
    We previously fixed the input to the IR being
    too long. That is no longer an issue. No we failed
    when saving the result in the internal task.
    
    So using the same approach (gzip + base64) here.
    
    In the failing release, the original string was
    7138 characters long. With the compression,
    it's 1873.
    
    Signed-off-by: Martin Malina <mmalina@redhat.com>

--- tasks/managed/filter-already-released-advisory-images/tests/mocks.sh
+++ tasks/managed/filter-already-released-advisory-images/tests/mocks.sh
@@ -7,9 +7,10 @@ function kubectl() {
   # The IR won't actually be acted upon, so mock it to return Success as the task wants
   if [[ "$*" == "get internalrequest "*"-o=jsonpath={.status.results}" ]]
   then
+    UNRELEASED=$(echo -n '["new-component"]' | gzip -c | base64 -w 0)
     echo '{
       "result": "Success",
-      "unreleased_components": ["new-component"],
+      "unreleased_components": "'"$UNRELEASED"'",
       "internalRequestPipelineRunName": "test-pipeline-run",
       "internalRequestTaskRunName": "test-task-run"
     }'
