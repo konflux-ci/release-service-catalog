d73925c505fffca453e353da173d65e14def97c3
Filip Nikolovski <fnikolov@redhat.com>
2025-08-21 10:50:46 +0200

    feat(RELEASE-1794): multi repo support in filter advisory images task
    Add support for the new repositories list format.
    
    Related: https://issues.redhat.com/browse/RELEASE-924
    
    Signed-off-by: Filip Nikolovski <fnikolov@redhat.com>

--- tasks/managed/filter-already-released-advisory-images/tests/mocks.sh
+++ tasks/managed/filter-already-released-advisory-images/tests/mocks.sh
@@ -21,7 +21,7 @@ function kubectl() {
   # The IR won't actually be acted upon, so mock it to return Success as the task wants
   if [[ "$*" == "get internalrequest "*"-o=jsonpath={.status.results}" ]]
   then
-    UNRELEASED=$(echo -n '["new-component"]' | gzip -c | base64 -w 0)
+    UNRELEASED=$(echo -n '["new-component", "multi-repo-component", "single-repo-component"]' | gzip -c | base64 -w 0)
     echo '{
       "result": "Success",
       "unreleased_components": "'"$UNRELEASED"'",
