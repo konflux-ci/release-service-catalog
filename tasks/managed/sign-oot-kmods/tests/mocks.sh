4cdf807ec3458fd83e35a351f76e8e5882b4cfb4
Enrique Belarte Luque <ebelarte@redhat.com>
2025-10-17 11:55:49 +0200

    fix: ensure signed .ko files replace unsigned ones in trusted artifacts
    Remove unsigned .ko files before copying
    signed ones back from signing server to prevent
    build-oot-kmods-rpms task from receiving unsigned files when
    buildRpm=true.
    
    Signed-off-by: Enrique Belarte Luque <ebelarte@redhat.com>

--- tasks/managed/sign-oot-kmods/tests/mocks.sh
+++ tasks/managed/sign-oot-kmods/tests/mocks.sh
@@ -33,13 +33,45 @@ function ssh() {
   esac
 }
 
+function rm() {
+  echo "Mock rm called with: $*"
+
+  echo "$*" >> "$(params.dataDir)/mock_rm.txt"
+
+  case "$*" in
+    "-f "*"/*.ko")
+      ;;
+    "-f "*signed-kmods*)
+      ;;
+    *)
+      echo "Error: Unexpected rm parameters: $*"
+      exit 1
+      ;;
+  esac
+}
+
 function scp() {
   echo "Mock scp called with: $*"
-  
+
   echo "$*" >> "$(params.dataDir)/mock_scp.txt"
 
   case "$*" in
     "-o UserKnownHostsFile=~/.ssh/known_hosts -o GSSAPIAuthentication=yes -o GSSAPIDelegateCredentials=yes"*)
+      # Check if this is the download command (contains *.ko in remote path)
+      if [[ "$*" == *"~/kmods/*.ko"* ]]; then
+        # This is the download command - extract destination directory (last argument)
+        local args=($*)
+        local dest_path="${args[-1]}"
+        echo "Mock scp: Creating signed files in destination: $dest_path"
+        if [ -d "$dest_path" ]; then
+          echo "SIGNED_MODULE1" > "$dest_path/mod1.ko"
+          echo "SIGNED_MODULE2" > "$dest_path/mod2.ko"
+          echo "Mock scp: Created signed files in $dest_path"
+        else
+          echo "Mock scp: Warning - destination directory $dest_path does not exist"
+        fi
+      fi
+      # For upload command, we don't need to do anything special
       ;;
     *)
       echo "Error: Incorrect scp parameters"
