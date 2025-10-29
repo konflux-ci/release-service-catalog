8d8f37c467fc17d33c9502a0b6978bb539c904c9
Elena German <elgerman@redhat.com>
2025-10-29 16:36:57 -0400

    fix(RELEASE-1973): create a dummy `trusted-ca` ConfigMap before run
    Signed-off-by: Elena German <elgerman@redhat.com>

--- tasks/managed/update-trusted-tasks/tests/pre-apply-task-hook.sh
+++ tasks/managed/update-trusted-tasks/tests/pre-apply-task-hook.sh
@@ -4,3 +4,8 @@ TASK_PATH=$1
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 yq -i '.spec.steps[1].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[1].script' $TASK_PATH
+
+# Create a dummy configmap for the trusted-ca volume (and delete it first if it exists)
+kubectl delete configmap trusted-ca --ignore-not-found
+kubectl create configmap trusted-ca --from-literal=ca-bundle.crt=dummy-cert
+
