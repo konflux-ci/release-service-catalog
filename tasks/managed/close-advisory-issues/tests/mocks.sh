87eaf4891ba63801f4bbdee0c5d981768258a816
Johnny Bieren <jbieren@redhat.com>
2026-03-16 14:18:01 -0400

    feat(KONFLUX-12331): support new jira server
    This commit updates the embargo-check, close-advisory-issues and
    populate-release-notes managed tasks to support the new red hat jira
    server.
    
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- tasks/managed/close-advisory-issues/tests/mocks.sh
+++ tasks/managed/close-advisory-issues/tests/mocks.sh
@@ -6,28 +6,28 @@ function curl-with-retry() {
   echo Mock curl called with: $* >&2
   echo $* >> "$(params.dataDir)/mock_curl.txt"
 
-  if [[ "$*" == *"Content-Type"*"https://issues.redhat.com/rest/api/2/issue/ISSUE-123/transitions" ]]
+  if [[ "$*" == *"-u team@domain.com:abcdefg"*"Content-Type"*"https://redhat.atlassian.net/rest/api/2/issue/ISSUE-123/transitions" ]]
   then
     :
-  elif [[ "$*" == *"Content-Type"*"https://issues.redhat.com/rest/api/2/issue/FAIL-999/transitions" ]]
+  elif [[ "$*" == *"-u team@domain.com:abcdefg"*"Content-Type"*"https://redhat.atlassian.net/rest/api/2/issue/FAIL-999/transitions" ]]
   then
     return 1
-  elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/FAIL-999/transitions" ]]
+  elif [[ "$*" == *"https://redhat.atlassian.net/rest/api/2/issue/FAIL-999/transitions" ]]
   then
     echo '{"transitions":[{"id":"91","name":"Closed","description":""},{"id":"11","name":"New"}]}'
-  elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/ISSUE-123/transitions" ]]
+  elif [[ "$*" == *"https://redhat.atlassian.net/rest/api/2/issue/ISSUE-123/transitions" ]]
   then
     echo '{"transitions":[{"id":"91","name":"Closed","description":""},{"id":"11","name":"New"}]}'
-  elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/NOCLOSE-555/transitions" ]]
+  elif [[ "$*" == *"https://redhat.atlassian.net/rest/api/2/issue/NOCLOSE-555/transitions" ]]
   then
     echo '{"expand":"transitions","transitions":[]}'
-  elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/CLOSED-987" ]]
+  elif [[ "$*" == *"https://redhat.atlassian.net/rest/api/2/issue/CLOSED-987" ]]
   then
     echo '{"fields":{"status":{"name":"Closed","id":"99"}}}'
-  elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/FAIL-999/comment" ]]
+  elif [[ "$*" == *"https://redhat.atlassian.net/rest/api/2/issue/FAIL-999/comment" ]]
   then
     echo '{}'
-  elif [[ "$*" == *"https://issues.redhat.com/rest/api/2/issue/NOCLOSE-555/comment" ]]
+  elif [[ "$*" == *"https://redhat.atlassian.net/rest/api/2/issue/NOCLOSE-555/comment" ]]
   then
     echo '{}'
   else
